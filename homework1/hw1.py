06f441b28e279d9edd38ccd4c6b29100535e1309
Alexis Daniels <danielsam3@vcu.edu>
2021-02-15 08:47:03 -0500

    change

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -9,7 +9,7 @@ def return_number_3():
 
 def return_string_vcu(): 
     """This function should return a string with the lowercase value of vcu"""
-    return_value = name.lower()
+    return_value = f"{string_vcu}"
     return return_value
 
 
