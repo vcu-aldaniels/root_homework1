a1e29354d467fe941afcb34d2047cf865fceab3f
Alexis Daniels <danielsam3@vcu.edu>
2021-02-15 01:10:59 -0500

    change

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -23,7 +23,7 @@ def return_lowercased_string(input_string):
     return return_value
 
 
-def return_without_starting_ending_whitespace("    input_string    "):
+def return_without_starting_ending_whitespace(    input_string    ):
     """You have a variable called input_string that is of type string.
     Return it but with the surrounding (left and right) whitespace stripped."""
 
