fa333053ea1a41281e2e2ed5fd049386a91c3141
Alexis Daniels <danielsam3@vcu.edu>
2021-02-14 23:55:25 -0500

    changes..

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -1,33 +1,33 @@
 """ This set of functions should return the appropriate data type"""
 
 
-def return_number_3():
+def return_number_3(): 3
     """ This function should return an integer with the value of 3"""
 
-    return_value = return_number_3()
+    return_value = 3
     return return_value
 
 
-def return_string_vcu():
+def return_string_vcu("vcu"): 
     """ This function should return a string with the lowercase value of vcu"""
 
-    return_value = return_string_vcu.lower()
+    return_value = vcu.lower()
     return return_value
 
 
-def return_lowercased_string(input_string):
+def return_lowercased_string("input_string"):
     """You have a variable called input_string that is of type string.
     Return it but the lowercase version of it."""
 
-    return_value = return_lowercased_string.lower(input_string)
+    return_value = input_string.lower()
     return return_value
 
 
-def return_without_starting_ending_whitespace(input_string):
+def return_without_starting_ending_whitespace("input_string"):
     """You have a variable called input_string that is of type string.
     Return it but with the surrounding (left and right) whitespace stripped."""
 
-    return_value = return_without_starting_ending_whitespace.strip(input_string)
+    return_value = input_string.strip()
     return return_value
 
 
