a4fa204c2b3aa4b1839715ac22d8f25069797e85
Alexis Daniels <danielsam3@vcu.edu>
2021-02-15 01:35:11 -0500

    I made changes!!

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -7,11 +7,15 @@ def return_number_3():
     return_value = 3
     return return_value
 
+
+
 def return_string_vcu(): 
     """ This function should return a string with the lowercase value of vcu"""
     
-    return_value = string.lower()
+    return_value = lower()
     return return_value
+
+
 
 def return_lowercased_string(input_string):
     """You have a variable called input_string that is of type string.
@@ -20,12 +24,16 @@ def return_lowercased_string(input_string):
     return_value = input_string.lower()
     return return_value
 
+
+
 def return_without_starting_ending_whitespace(input_string):
     """You have a variable called input_string that is of type string.
     Return it but with the surrounding (left and right) whitespace stripped."""
 
     return_value = input_string.strip()
     return return_value
+
+
 
 def return_addition(first_number, second_number):
     """ Return the two numbers added together. """
