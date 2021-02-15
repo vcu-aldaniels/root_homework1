8956da98b54d664b3964973b44bf1bdb34074b53
Alexis Daniels <danielsam3@vcu.edu>
2021-02-15 00:56:26 -0500

    changes!!

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -8,7 +8,7 @@ def return_number_3():
     return return_value
 
 
-def return_string_vcu(vcu): 
+def return_string_vcu(): 
     """ This function should return a string with the lowercase value of vcu"""
 
     return_value = vcu.lower()
@@ -27,7 +27,7 @@ def return_without_starting_ending_whitespace(input_string):
     """You have a variable called input_string that is of type string.
     Return it but with the surrounding (left and right) whitespace stripped."""
 
-    return_value = input_string.lstrip()
+    return_value = input_string.strip()
     return return_value
 
 
