d03a02d91628c52f858608e954f3d0b8f18acd6b
Alexis Daniels <danielsam3@vcu.edu>
2021-02-15 00:00:24 -0500

    I made some changes.

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -1,7 +1,7 @@
 """ This set of functions should return the appropriate data type"""
 
 
-def return_number_3(): 3
+def return_number_3(): 
     """ This function should return an integer with the value of 3"""
 
     return_value = 3
