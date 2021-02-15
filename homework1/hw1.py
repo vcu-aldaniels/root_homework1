df16b82b1f550e3cf5b47ebc6940c8089c5488dd
Alexis Daniels <danielsam3@vcu.edu>
2021-02-15 09:04:44 -0500

    changes

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -1,6 +1,5 @@
 """This set of functions should return the appropriate data type"""
 
-
 def return_number_3(): 
     """This function should return an integer with the value of 3"""
     return_value = 3
